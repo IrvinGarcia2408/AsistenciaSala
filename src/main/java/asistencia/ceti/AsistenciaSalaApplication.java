b44ef96df36b9f47134652fe9f34a64c4eb02bc2
IrvinGarcia2408 <garciaramosirvingefren08@gmail.com>
2022-03-08 16:30:25 -0600

    Subiendo el proyecto por primera vez.

---
+++ src/main/java/asistencia/ceti/AsistenciaSalaApplication.java
@@ -0,0 +1,13 @@
+package asistencia.ceti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciaSalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistenciaSalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/asistencia/edu/mx/AsistenciaSala/AsistenciaSalaApplicationTests.java
@@ -0,0 +1,13 @@
+package asistencia.edu.mx.AsistenciaSala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaSalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
