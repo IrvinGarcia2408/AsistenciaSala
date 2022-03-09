bc21d566a1a40abe3aed7023302d7d82b3d4c46c
IrvinGarcia2408 <garciaramosirvingefren08@gmail.com>
2022-03-09 16:23:24 -0600

    Sin cambios

--- src/main/java/asistencia/ceti/AsistenciaSalaApplication.java
+++ src/main/java/asistencia/ceti/AsistenciaSalaApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AsistenciaSalaApplication {
-
+	// token github
+	//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
 	public static void main(String[] args) {
 		SpringApplication.run(AsistenciaSalaApplication.class, args);
 	}
