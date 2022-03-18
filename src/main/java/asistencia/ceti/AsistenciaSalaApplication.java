8772c4933938c60afd7289196c5a28ce3cd6752d
Edson <santosedson515@gmail.com>
2022-03-18 11:22:43 -0600

    Añadiendo nuevas funcionalidades, de editar, y eliminar

--- src/main/java/asistencia/ceti/AsistenciaSalaApplication.java
+++ src/main/java/asistencia/ceti/AsistenciaSalaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AsistenciaSalaApplication {
 	// token github
-	//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
+	// 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
 	public static void main(String[] args) {
 		SpringApplication.run(AsistenciaSalaApplication.class, args);
 	}
